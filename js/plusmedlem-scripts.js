a43922650c3420a420f20828d5e2c2534bd99663
Alex Demyan <107802234+DemGam@users.noreply.github.com>
2024-10-04 10:51:47 +0300

    Add files via upload

--- js/plusmedlem-scripts.js
+++ js/plusmedlem-scripts.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Animation on scroll
   const the_animation = document.querySelectorAll(".animated");
-  if (the_animation) {
+  if (the_animation.length) {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -16,36 +16,50 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     for (let i = 0; i < the_animation.length; i++) {
       const elements = the_animation[i];
-
       observer.observe(elements);
     }
   }
 });
 
 window.addEventListener("load", function () {
-  const header = document.querySelector(".pml-header");
-  let lastScrollTop = 0;
+  var scrolling;
+  var position = 0;
+  var delta = 5;
+  var header = document.querySelector(".pml-header");
+  var docHeight = document.documentElement.scrollHeight;
+  var winHeight = window.innerHeight;
 
-  window.addEventListener("scroll", function () {
-    toggleHeaderOnScroll();
+  window.addEventListener("scroll", function (event) {
+    scrolling = true;
   });
 
-  function toggleHeaderOnScroll() {
-    let currentScrollPosition =
+  setInterval(function () {
+    if (scrolling) {
+      scrollFunction();
+      scrolling = false;
+    }
+  }, 250);
+
+  function scrollFunction() {
+    var currentScrollPosition =
       window.pageYOffset || document.documentElement.scrollTop;
-    if (currentScrollPosition > lastScrollTop) {
+
+    if (Math.abs(position - currentScrollPosition) <= delta) return;
+
+    if (
+      currentScrollPosition > position &&
+      currentScrollPosition > header.offsetHeight
+    ) {
       header.classList.add("hide");
     } else {
-      header.classList.remove("hide");
+      if (currentScrollPosition + winHeight < docHeight) {
+        header.classList.remove("hide");
+      }
     }
-    lastScrollTop = Math.max(currentScrollPosition, 0);
+    position = currentScrollPosition;
   }
 });
+
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
-  if (header && header.classList.contains("hide")) {
-    header.classList.remove("hide");
-  }
 };
-
-
